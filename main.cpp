ebe3052ec27a7a589ef59302b7c7c64207ab06c0
Tom smith <1101282468@qq.com>
2016-01-10 15:34:45 +0800

    对代码进行了重构，但还有一些问题没有解决

--- main.cpp
+++ main.cpp
@@ -4,88 +4,95 @@
 #include <ctime>
 #include <cstring>
 #include <unistd.h>
-const int live=3,die=2,l=78,s=78;
-bool now[s][l],past[s][l];
-template<typename T>
-inline void swap(T a,T b){
-	T c=a;
-	a=b;
-	b=c;
-}
-void print(bool flag){
-	if(flag){
+const int live=3,maintain=2,l=30,s=30;
+struct cell{
+	bool now,past;
+};
+struct screen{
+    cell pixel[s][l];
+    void print_n(){
+        for(int i=0;i<s;i++){
+            for(int j=0;j<l;j++){
+                if(pixel[i][j].now){
+                    printf("●");
+                }else{
+                    printf("○");
+                }
+            }
+			printf("\n");
+        }
+    }
+    void print_p(){
+        for(int i=0;i<s;i++){
+            for(int j=0;j<l;j++){
+                if(pixel[i][j].past){
+                    printf("●");
+                }else{
+                    printf("○");
+                }
+            }
+			printf("\n");
+        }
+    }
+    inline int count(int x,int y){
+    	return (pixel[(x+1)%s][(y+1)%l].past
+    			+pixel[x][(y+1)%l].past
+    			+pixel[(x-1+s)%s][(y+1)%l].past
+    			+pixel[(x+1)%s][y].past
+    			+pixel[(x-1+s)%s][y].past
+    			+pixel[(x+1)%s][(y-1+l)%l].past
+    			+pixel[x][(y-1+l)%l].past
+    			+pixel[(x-1+s)%s][(y-1+l)%l].past
+    	);
+    }
+    inline bool liveordie(int x,int y){
+        int temp=count(x,y);
+        if(temp==live){
+            return true;
+        }else if(temp==maintain){
+            return pixel[x][y].past;
+        }else{
+            return false;
+        }
+    }
+	void start(){
 		for(int i=0;i<s;i++){
 			for(int j=0;j<l;j++){
-				if(now[i][j]){
-					printf("●");
+				if(rand()%2==1){
+					pixel[i][j].now=true;
 				}else{
-					printf("○");
+					pixel[i][j].now=false;
 				}
 			}
-			printf("\n");
 		}
-	}else{
+	}
+	void print_info(){
+		printf("规则 生%d 保持%d\n",live,maintain);
+		printf("长%d 宽%d\n",l,s);
+	}
+	void change(){
 		for(int i=0;i<s;i++){
 			for(int j=0;j<l;j++){
-				if(past[i][j]){
-					printf("●");
-				}else{
-					printf("○");
-				}
+				pixel[i][j].past=pixel[i][j].now;
 			}
-			printf("\n");
 		}
 	}
-}
-inline int count(int a,int b){
-	return (past[(a+1)%s][(b+1)%l]
-			+past[a][(b+1)%l]
-			+past[(a-1+s)%s][(b+1)%l]
-			+past[(a+1)%s][b]
-			+past[(a-1+s)%s][b]
-			+past[(a+1)%s][(b-1+l)%l]
-			+past[a][(b-1+l)%l]
-			+past[(a-1+s)%s][(b-1+l)%l]
-	);
-}
-inline bool liveordie(int a,int b){
-	int temp=count(a,b);
-	if(temp==live){
-		return true;
-	}else if(temp==die){
-		return past[a][b];
-	}else{
-		return false;
+	void calc(){
+		for(int i=0;i<s;i++){
+			for(int j=0;j<l;j++){
+				pixel[i][j].now=liveordie(i,j);
+			}
+		}
 	}
-}
+};
 int main(void){
-	int timeseed=time(NULL);
-	printf("%d\n",timeseed);
-	srand(timeseed);
-	printf("规则 生%d 死%d\n",live,die);
-	printf("长%d 宽%d\n",l,s);
-	for(int i=0;i<s;i++){
-		for(int j=0;j<l;j++){
-			if(rand()%2==1){
-				now[i][j]=true;
-			}else{
-				now[i][j]=false;
-			}
-		}
-	}
+	screen a;
+	a.print_info();
+	a.start();
 	while(true){
-		print(1);
-		printf("\n");
-		for(int i=0;i<s;i++){
-			for(int j=0;j<l;j++){
-				past[i][j]=now[i][j];
-			}
-		}
-		for(int i=0;i<s;i++){
-			for(int j=0;j<l;j++){
-				now[i][j]=liveordie(i,j);
-			}
-		}
+		a.print_n();
+		a.change();
+		a.calc();
 		sleep(1);
 		system("clear");
 	}
